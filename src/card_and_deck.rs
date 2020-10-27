280e273bd422371c27deeaf06d9238f903d91725
mnussbaum <michaelnussbaum08@gmail.com>
2020-10-27 00:02:31 -0700

    Implement card to unicode character mapping

--- src/card_and_deck.rs
+++ src/card_and_deck.rs
@@ -1,5 +1,7 @@
 use rand::seq::SliceRandom;
 use rand::thread_rng;
+use std::char;
+use std::fmt;
 
 #[derive(Debug)]
 pub struct Deck {
@@ -47,6 +49,17 @@ pub enum Suit {
     Spade,
 }
 
+impl Suit {
+    pub fn unicode_code_point(&self) -> u32 {
+        match self {
+            Suit::Spade => 0xA0,
+            Suit::Heart => 0xB0,
+            Suit::Diamond => 0xC0,
+            Suit::Club => 0xD0,
+        }
+    }
+}
+
 #[derive(Debug, PartialOrd, PartialEq)]
 pub enum Rank {
     Two = 2,
@@ -83,10 +96,54 @@ impl Rank {
             _ => panic!("Unknown rank: {}", rank),
         }
     }
+
+    pub fn unicode_code_point(&self) -> u32 {
+        match self {
+            Rank::Two => 0x2,
+            Rank::Three => 0x3,
+            Rank::Four => 0x4,
+            Rank::Five => 0x5,
+            Rank::Six => 0x6,
+            Rank::Seven => 0x7,
+            Rank::Eight => 0x8,
+            Rank::Nine => 0x9,
+            Rank::Ten => 0xA,
+            Rank::Jack => 0xB,
+            Rank::Queen => 0xD,
+            Rank::King => 0xE,
+            Rank::Ace => 0x1,
+        }
+    }
 }
 
-#[derive(Debug)]
 pub struct Card {
     pub suit: Suit,
     pub rank: Rank,
+}
+
+impl fmt::Debug for Card {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self)
+    }
+}
+
+const PLAYING_CARD_UNICODE_CODE_POINT_LOWER_BOUND: u32 = 0x1F000;
+
+impl fmt::Display for Card {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{}",
+            char::from_u32(
+                PLAYING_CARD_UNICODE_CODE_POINT_LOWER_BOUND
+                    + self.suit.unicode_code_point()
+                    + self.rank.unicode_code_point()
+            )
+            .expect(&format!(
+                "Invalid card to unicode conversion for: {:#?} {:#?}",
+                self.rank, self.suit
+            ))
+            .to_string(),
+        )
+    }
 }
